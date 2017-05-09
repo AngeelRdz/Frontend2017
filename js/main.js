206277feedcbce5497b4b0c25facb7b7a9bd134b
AngeelRdz <ame_mi_amor@hotmail.com>
2017-05-08 22:41:33 -0500

    Frontend actualizado
    - Se agrego un slider básico con Javascript y CSS automatico.
    - Se esta agregando la sección del formulario para enviar correos,
    funcionando correctamente.
    - Falta mejorar Frontend, estilos del formulario y funciones de
    validaciones en el proceso de mandar los correos.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,51 @@
 
 	let sticky = false
 
+	//Iniciamos en la posicion 0 (para el slider)
+	let currentPosition = 0
+
+	//Se crea una constante del numero total de imagenes menos 1
+	//Se crea una conexion de javascript con html5 a traves de un meta
+	const imageCounter = $("[data-name='image-counter']").attr("content")
+	console.log(imageCounter)
+
+	//Constante para el correo electronico
+	const email = "rodriguez_1409@hotmail.com"
+
+	//Para mandar a imprimir en consola el formulario
+	$("#contact-form").on("submit", function(ev){
+		
+		ev.preventDefault()
+
+		sendForm($(this))
+
+		return false
+	})
+
 	//Se quita la clase hidden del id 
 	$("#sticky-navigation").removeClass("hidden")
 	//Se agrega esta linea en el id para quitar la navegacion en cero milisegundos
 	$("#sticky-navigation").slideUp(0)
+
+	//Intervalo para el slide
+	setInterval(()=>{
+
+		if (currentPosition < imageCounter) {
+			currentPosition++
+		}else{
+			currentPosition = 0
+		}
+
+		//console.log("Hola Mundo");
+		$("#gallery .inner").css({
+			//left: "-100%"
+			//Ya no se manda estetico
+			left: "-" + currentPosition * 100 + "%"
+		})
+	},3000)
+
+	//Otra manera de hacerlo
+	//setInterval(diHola,1000)
 
 
 	//Al cargar la pagina el tamaño de la ventana se muestra
@@ -79,6 +120,19 @@
 		$("#sticky-navigation").slideUp("100")
 	}
 
+	function sendForm($form){
+		//console.log($form.formObject())
+		$.ajax({
+		    url: $form.attr("action"), 
+		    method: "POST",
+		    data: $form.formObject(),
+		    dataType: "json",
+		    success: function(){
+		    	alert("Todo salio bien")
+		    }
+		})
+	}
+
 	function isInBottom(){
 		//Constante ya no va a cambiar al obtener el valor
 		const $description = $("#description")
@@ -88,6 +142,10 @@
 		//Nos devuelve true o false - falso cuando estamos arriba y verdadero cuando estamos abajo (tope de la ventana)
 		//Si es masyor al tamaño de la ventana menos descripcionheight por 5.5 es para darle mas espacio
 		return $(window).scrollTop() > $(window).height() - (descriptionHeight * 5.5)
+	}
+
+	function diHola(){
+		console.log("Hola mundito 2")
 	}
 
 })()
---
+++ js/steps.js
@@ -0,0 +1,23 @@
+;(function(){
+
+	//Nos va a decir que formulario va a procesar
+	const selector = "#contact-form"
+
+	$(selector).find(".input").on("change",()=>{
+		
+	})
+
+	//Funciones de ayuda
+	function validar_formulario(){
+
+	}
+
+	function es_valido_formulario(){
+
+	}
+
+	function enfocar_nuevo_paso(){
+
+	}
+
+})
